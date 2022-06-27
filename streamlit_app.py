c632a0940fde042660cc2c124b43ef28fed2b5df
diagnostikon <eddie.vega@gmail.com>
2022-06-27 16:41:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,35 +1,60 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+from pages import search_engine_page, document_page
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if "selected_record" not in st.session_state:
+    st.session_state["selected_record"] = None
 
-    Point = namedtuple('Point', 'x y')
-    data = []
 
-    points_per_turn = total_points / num_turns
+def set_record(record):
+    st.session_state["selected_record"] = record
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if not st.session_state["selected_record"]:  # search engine page
+    st.set_page_config(
+        page_title="HuggingFace Search Engine",
+        page_icon="🔎",
+        layout="wide",
+        initial_sidebar_state="auto",
+        # menu_items={
+        #     "Get Help": "https://www.extremelycoolapp.com/help",
+        #     "Report a bug": "https://www.extremelycoolapp.com/bug",
+        #     "About": "# This is a header. This is an *extremely* cool app!",
+        # },
+    )
+    search_engine_page()
+
+else:  # a record has been selected
+    document_page()
+
+
+st.markdown(
+    """<style>
+a:link , a:visited{
+color: blue;
+background-color: transparent;
+text-decoration: underline;
+}
+
+a:hover,  a:active {
+color: red;
+background-color: transparent;
+text-decoration: underline;
+}
+
+.footer {
+# position: fixed;
+left: 0;
+bottom: 0;
+width: 100%;
+background-color: white;
+color: black;
+text-align: center;
+}
+</style>
+<div class="footer">
+<p>Made with ❤️ by <b>Nouamane Tazi</b></p>
+</div>
+""",
+    unsafe_allow_html=True,
+)
